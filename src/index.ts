5c72452f7032a1d0f4c98e44af2553920ffc719d
wadahkode <mvp.dedefilaras@gmail.com>
2025-04-14 15:43:22 +0000

    menghapus keterangan otomatis berjalan dari jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 /**
- * Titik awal dari pustaka AIDOMX.
- *
- * File ini bertanggung jawab memulai proses inisialisasi AIDOMX berdasarkan environment.
- * Secara default, akan berjalan di browser menggunakan konfigurasi standar.
- * Framework lain seperti React atau Vue bisa diintegrasikan melalui sistem environment modular.
- *
- * Cocok digunakan pada proyek frontend modern yang ingin mendukung AI-driven styling
- * secara fleksibel dan scalable.
+ * Aidomx Entry File
  *
  * @file src/index.ts
  * @created 2025-04-07
